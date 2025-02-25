37f75da98d4d7745051149cc75774f2015266b0d
mitchtembo <42540252+mitchtembo@users.noreply.github.com>
2025-02-25 12:33:19 +0200

    fullstack v 0 0 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,17 @@
-import Link  from "next/link";
+import connectDB from "@/config/database";
+import Link from "next/link";
 import Hero from "./components/Hero";
 import InfoBoxes from "./components/InfoBoxes";
 
-const Homepage = () => {
+const Homepage = async () => {
+  await connectDB();
+  
   return (
     <div>
       <Hero />
       <InfoBoxes />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
