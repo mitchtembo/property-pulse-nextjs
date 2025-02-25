4cd5af46d95531a6a718e52b55ef7b8338a232a9
mitchtembo <42540252+mitchtembo@users.noreply.github.com>
2025-02-25 23:13:33 +0200

    properties page db fix

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,22 +1,39 @@
-import properties from '@/properties.json';
-import PropertyCard from '../components/PropertyCard';
-const PropertiesPage = () => {
-  return (
-    <section className="px-4 py-6">
-      <div className="container-xl md:container m-auto">
-        {properties.length === 0 ? (
-          <p>No properties found.</p>
-        ) : (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {properties.map((property) => (
-            <PropertyCard key={property._id} property={property} />
-          ))}
-        </div>
+import PropertyCard from '@/app/components/PropertyCard';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
-        )}
-      </div>
-    </section>
-  )
-}
+const PropertiesPage = async () => {
+  try {
+    await connectDB();
+    console.log('Database connected in properties page');
+
+    const properties = await Property.find({}).lean();
+    console.log('Properties fetched:', properties);
+
+    if (!properties) {
+      console.log('No properties found');
+      return <p>No properties found</p>;
+    }
+
+    return (
+      <section className="px-4 py-6">
+        <div className="container-xl md:container m-auto">
+          {properties.length === 0 ? (
+            <p>No properties found.</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  } catch (error) {
+    console.error('Error fetching properties:', error);
+    return <p>Error loading properties</p>;
+  }
+};
 
 export default PropertiesPage;
---
+++ models/Users.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"]
+    },
+    username: {
+        type: String,
+        unique: [true, "Username already exists"],
+        required: [true, "Username is required"],
+    },
+
+    image: {
+        type: String,
+    },
+    bookmarks: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Property"
+        }
+    ]
+}, {
+    timestamps: true,
+}
+);
+
+
+const User = models.User || model("User", userSchema);
+
+export default User;
